892e13c5dcd943700257e6870ae0e7f10f1f129e
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2019-08-02 14:18:37 -0300

    Add versioning publish (2012 R2 and 2016)

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,10 @@
 @echo off
+REM Windows 2016
+set RUNTIME="win10-x64"
+REM Windows 2012 R2
+REM set RUNTIME="win81-x64"
+
+REM Publish
 dotnet clean
-dotnet publish -c Release --runtime win10-x64
-explorer %~dp0bin\Release\netcoreapp2.1\win10-x64\publish\
+dotnet publish -c Release --runtime %RUNTIME%
+explorer %~dp0bin\Release\netcoreapp2.1\%RUNTIME%\publish\
